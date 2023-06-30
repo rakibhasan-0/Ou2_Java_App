62ea5f3d21a765c1db25d0591a469fb777e77047
Rakib <Alvin15148@gmail.com>
2023-06-30 11:38:40 +0200

    updated

---
+++ uppgift_1/src/Build.java
@@ -0,0 +1,20 @@
+public class Build {
+    public static void main(String[] args){
+
+        DogBuilder builder = new Spike();
+        Director director = new Director(builder);
+
+        director.createDogElements();
+        Dog dog = director.getDog();
+
+        System.out.println("dog name ::"+ dog.getName());
+        System.out.println("dog age ::" + dog.getAge());
+        System.out.println("dog type ::"+ dog.getType());
+
+        for(String toy : dog.getToys()){
+            System.out.println(toy);
+        }
+
+
+    }
+}
